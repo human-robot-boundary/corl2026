5d1bc23e46cdc4e0f0ce74ffb0de66b6b7e4a782
Yoshiki Obinata <mqcmd196@hotmail.co.jp>
2026-05-29 12:21:52 +0900

    update

--- assets/js/config.js
+++ assets/js/config.js
@@ -1,23 +1,23 @@
 const project_name = `Designing the Human-Robot Boundary: From Human Guidance to Robot Autonomy in Real-World Deployment`
-const proj_small_caps = `<span class=”small-caps”></span>`
-const conference_details = [‘CoRL 2026’, ‘https://www.corl.org/’, ‘TBD’, ‘’, ‘’]
+const proj_small_caps = `<span class="small-caps"></span>`
+const conference_details = ['CoRL 2026', 'https://www.corl.org/', 'TBD', '', '']
 const workshop_date = `TBD`
 
 // Invited speakers to be announced
 const talk_speaker_details = {
-  // ‘tbd’: [‘TBD’, ‘’, ‘’, ‘’, ‘’,
-  //   ‘’,
-  //   ‘’],
+  // 'tbd': ['TBD', '', '', '', '',
+  //   '',
+  //   ''],
 }
 
 const organizers_talk_details = {
 }
 
 const organizers_details = [
-  [‘Yoshiki Obinata’, ‘assets/images/organizers/obinata.jpg’, ‘PhD Student’, ‘The University of Tokyo, Japan’, ‘https://obinata0922.github.io/’],
-  [‘Kento Kawaharazuka’, ‘assets/images/organizers/kawaharazuka.png’, ‘Lecturer’, ‘The University of Tokyo, Japan’, ‘https://haraduka.github.io’],
-  [‘Kashu Yamazaki’, ‘assets/images/organizers/yamazaki.jpg’, ‘PhD Student’, ‘Carnegie Mellon University, USA’, ‘’],
-  [‘Mingyo Seo’, ‘assets/images/organizers/seo.jpg’, ‘PhD Student’, ‘The University of Texas at Austin, USA’, ‘’],
+  ['Yoshiki Obinata', 'assets/images/organizers/obinata.jpg', 'Project Assistant Professor', 'The University of Tokyo, Japan', 'https://www.yoshiki-obinata.com/'],
+  ['Kento Kawaharazuka', 'assets/images/organizers/kawaharazuka.png', 'Lecturer', 'The University of Tokyo, Japan', 'https://haraduka.github.io'],
+  ['Kashu Yamazaki', 'assets/images/organizers/yamazaki.jpg', 'PhD Student', 'Carnegie Mellon University, USA', 'https://kashu7100.github.io/'],
+  ['Mingyo Seo', 'assets/images/organizers/seo.jpg', 'PhD', 'The University of Texas at Austin, USA', 'https://mingyoseo.com/'],
 ]
 
 const accepted_presentations = [
